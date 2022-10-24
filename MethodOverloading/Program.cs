98f3b842625ba0a2c939c60de3be80c5bd4dfabd
kyle jenne <strongagent1@gmail.com>
2022-10-24 15:58:49 -0400

    MethodOverloading KJ

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -1,10 +1,40 @@
-﻿namespace MethodOverloading
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace MethodOverloading
 {
     public class Program
     {
+        static int Add(int x, int y)
+        { return x + y; } 
+           
+
+        static decimal Add(decimal x, decimal y)
+        { return x + y; }
+
+            
+        static string Add(int x, int y, bool z)
+        {
+            if (z && (x + y) != 1)
+            {
+                return $"{x + y} dollars";
+            }
+            else if (z && (y + x) == 1)
+            {
+                return $"{x + y} dollar";
+
+            }else
+            {
+                return $"{x+y}";
+            }
+
+
+        }        
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var answer = Add(0, 1, true);
+            Console.WriteLine(answer);
         }
     }
 }
